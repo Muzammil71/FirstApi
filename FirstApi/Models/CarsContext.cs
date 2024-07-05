133b89126ae36c2ac2de547e364c1f8a5a58d13e
Muzammil71 <zamiikhan713@gmail.com>
2024-07-05 18:34:05 +0500

    FirstWebAPI develop

---
+++ FirstApi/Models/CarsContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace FirstAPI.Models;
+
+public class CarsContext : DbContext
+{
+    public CarsContext(DbContextOptions<CarsContext> options) : base(options)
+    {
+    }
+    public DbSet<Cars> Cars { get; set; } = null!;
+}
